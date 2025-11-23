62d902193c5a5bf3614525d246a8061e44d00fbd
Varaprasad-22 <160947090+Varaprasad-22@users.noreply.github.com>
2025-11-23 23:56:03 +0530

    intial Commit

---
+++ src/main/java/com/flightapp/repository/BookingRepository.java
@@ -0,0 +1,7 @@
+package com.flightapp.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BookingRepository extends MongoRepository<T, ID> {
+
+}
---
+++ src/main/java/com/flightapp/repository/FlightRepository.java
@@ -0,0 +1,7 @@
+package com.flightapp.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface FlightRepository extends MongoRepository<T,ID> {
+
+}
